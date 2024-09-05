c1eb356e4a1ab744c24bfcf93a35b4602478c738
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-09-05 13:49:40 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,41 @@
+import asyncio, os, re, random, pytz, requests, json
 
+async def react_msg(client, message):
+    emojis = [
+        "👍",
+        "❤",
+        "🔥",
+        "🥰",
+        "👏",
+        "😁",
+        "🤔",
+        "😱",
+        "🎉",
+        "🤩",
+        "🤡",
+        "😍",
+        "❤‍🔥",
+        "🌚",
+        "🤣",
+        "⚡",
+        "🏆",
+        "🤨",
+        "😐",
+        "😈",
+        "🤓",
+        "👻",
+        "😇",
+        "🤝",
+        "🤗",
+        "🫡",
+        "🎅",
+        "🎄",
+        "🆒",
+        "😘",
+        "😎",
+    ]
+    rnd_emoji = random.choice(emojis)
+    await client.send_reaction(
+        chat_id=message.chat.id, message_id=message.id, emoji=rnd_emoji, big=True
+    )
+    return
