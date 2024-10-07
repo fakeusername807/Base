a4e465cfbcdd041a07710de440c4d33baa035831
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-10-07 15:28:20 +0530

    Update Reaction.py

--- Rkn_Bots/Reaction.py
+++ Rkn_Bots/Reaction.py
@@ -5,10 +5,10 @@
 # Developer @RknDeveloperr
 
 from pyrogram import Client, filters, errors, types
-from config import Rkn_Bots
+from config import Rkn_Bots, AUTH_CHANNEL
 import asyncio, re, time, sys, random
 from .database import total_user, getid, delete, addCap, updateCap, insert, chnl_ids
-from pyrogram.errors import FloodWait
+from pyrogram.errors import *
 from pyrogram.types import *
 from utils import react_msg 
 
@@ -22,6 +22,18 @@ buttons = [[
 group_buttons = [[InlineKeyboardButton('✇ Click To Start Me ✇', url="http://t.me/Reaction_99bot?start=True")
              ],[
                   InlineKeyboardButton('✇ Uᴘᴅᴀᴛᴇs ✇', url="https://t.me/HGBOTZ")]] 
+
+async def is_subscribed(bot, query, channel):
+    btn = []
+    for id in channel:
+        chat = await bot.get_chat(int(id))
+        try:
+            await bot.get_chat_member(id, query.from_user.id)
+        except UserNotParticipant:
+            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
+        except Exception as e:
+            pass
+    return btn
 
 @Client.on_message(filters.private & filters.user(Rkn_Bots.ADMIN)  & filters.command(["stats"]))
 async def all_db_users_here(client, message):
@@ -76,7 +88,19 @@ async def restart_bot(b, m):
     
 @Client.on_message(filters.command("start") & filters.private)
 async def start_cmd(bot, message):
-    await react_msg(bot, message)
+    if AUTH_CHANNEL:
+        try:
+            btn = await is_subscribed(client, message, AUTH_CHANNEL)
+            if btn:
+                username = (await client.get_me()).username
+                if message.command[1]:
+                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start={message.command[1]}")])
+                else:
+                    btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start=true")])
+                await message.reply_text(text=f"<b>👋 Hello {message.from_user.mention},\n\nPlease join the channel then click on try again button. 😇</b>", reply_markup=InlineKeyboardMarkup(btn))
+                return
+        except Exception as e:
+            print(e)
     user_id = int(message.from_user.id)
     reply_markup=InlineKeyboardMarkup(buttons)
     await insert(user_id)
