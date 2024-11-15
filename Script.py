d8f554c5ca8c8d39ce3220b21dd7338ed9f9f75b
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-11-15 12:33:43 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,30 @@
+class script(object):
+    ABOUT_TXT = """<b><blockquote>⍟───[ MY ᴅᴇᴛᴀɪʟꜱ ]───⍟</blockquote>
+    
+‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/AutoCaption_HGbot>AutoCaption_HGbot</a>
+‣ ᴍʏ ʙᴇsᴛ ғʀɪᴇɴᴅ : <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
+‣ maintenar: <a href='https://t.me/hgbotz'>HGBOTZ</a> 
+‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a> 
+‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 3</a> 
+‣ ᴅᴀᴛᴀ ʙᴀsᴇ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏ ᴅʙ</a> 
+‣ ʙᴏᴛ sᴇʀᴠᴇʀ : <a href='https://heroku.com'>ʜᴇʀᴏᴋᴜ</a> 
+‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ2.7.1 [sᴛᴀʙʟᴇ]></b>
+"""
+
+    HELP_TXT = """<b>𝙱𝚞𝚍𝚍𝚢 𝙽𝚘𝚝 𝙰 𝙷𝚊𝚛𝚍 𝙿𝚛𝚘𝚌𝚎𝚜𝚜 😂
+𝙸𝚝 𝙹𝚞𝚜𝚝 𝚂𝚒𝚖𝚙𝚕𝚎 𝚁𝚎𝚊𝚌𝚝𝚒𝚘𝚗 𝙱𝚘𝚝 𝚠𝚒𝚝𝚑 𝚂𝚘𝚖𝚎 𝚃𝚒𝚖𝚎𝚙𝚊𝚊𝚜 𝙶𝚊𝚖𝚎
+𝙾𝙽𝙻𝚈 𝙼𝚊𝚔𝚎 𝙼𝚎 𝙰𝚍𝚖𝚒𝚗 𝙸𝚗 𝚈𝚘𝚞𝚛 𝙶𝚛𝚘𝚞𝚙 𝙾𝚛 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 𝙵𝚘𝚛 𝙼𝚊𝚐𝚒𝚌𝚊𝚕 𝚁𝚎𝚊𝚌𝚝𝚒𝚘𝚗𝚜 💞
+𝙾𝚛 𝙳𝚘 /𝚍𝚒𝚌𝚎  /𝚝𝚑𝚛𝚘𝚠 𝚎𝚝𝚌 𝚖𝚘𝚛𝚎 𝚌𝚖𝚗𝚍𝚜 𝚠𝚑𝚒𝚌𝚑 𝚜𝚑𝚘𝚠 𝚞 𝚒𝚗 𝚜𝚝𝚊𝚛 𝚝𝚎𝚡𝚝 𝚊𝚗𝚍 𝚎𝚗𝚓𝚘𝚢
+
+<𝚋><𝚋𝚕𝚘𝚌𝚔𝚚𝚞𝚘𝚝𝚎>𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍 𝙱𝚢 <𝚊 𝚑𝚛𝚎𝚏='𝚑𝚝𝚝𝚙𝚜://𝚝.𝚖𝚎/𝚑𝚐𝚋𝚘𝚝𝚣'>𝙷𝙶𝙱𝙾𝚃ᶻ 🦋"""
+
+    HOME_TXT = """Hᴇʟʟᴏ 😎 
+<b><blockquote>ɪ ᴀᴍ  ᴘᴏᴡᴇʀꜰᴜʟʟ AUTO REACTION ʙᴏᴛ ᴊᴜꜱᴛ Make Admin in Your Group/Chat to see Magic☜</blockquote></b> 
+<b><blockquote expandable>For Fun Use These Commands
+◉ /dice
+◉ /arrow
+◉ /goal
+◉ /luck
+◉ /throw
+◉ /bowling
+◉ /tenpins</blockquote></b>"""
