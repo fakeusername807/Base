e69fc6ce8c5185fb976d391f5420649f181d7435
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2025-05-25 13:07:53 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,15 +1,5 @@
 class script(object):
     START_TXT = """𝗛𝗲𝗹𝗹𝗼 {} 💞
-    ɪ ᴀᴍ sɪᴍᴘʟᴇ ʙᴜᴛ ᴘᴏᴡᴇʀꜰᴜʟʟ ᴀᴜᴛᴏ ʀᴇᴀᴄᴛɪᴏɴ ʙᴏᴛ.
-ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴀs ᴀ ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴏʀ ɢʀᴏᴜᴘ ᴛʜᴇɴ sᴇᴇ ᴍʏ ᴘᴏᴡᴇʀ
-<b><blockquote expandable>For Fun Use These Commands
-◉ /dice
-◉ /arrow
-◉ /goal
-◉ /luck
-◉ /throw
-◉ /bowling
-◉ /tenpins</blockquote></b>
 <b><blockquote>Maintained By <a href='https://t.me/hgbotz'>𝙷𝙶𝙱𝙾𝚃ᶻ 🦋</a></blockquote></b>"""
 
     ABOUT_TXT = """<b><blockquote>⍟───[ MY ᴅᴇᴛᴀɪʟꜱ ]───⍟</blockquote>
@@ -24,23 +14,7 @@ class script(object):
 ‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ2.7.1 [sᴛᴀʙʟᴇ]></b>
 """
 
-    HELP_TXT = """<b>𝙱𝚞𝚍𝚍𝚢 𝙽𝚘𝚝 𝙰 𝙷𝚊𝚛𝚍 𝙿𝚛𝚘𝚌𝚎𝚜𝚜 😂
-𝙸𝚝 𝙹𝚞𝚜𝚝 𝚂𝚒𝚖𝚙𝚕𝚎 𝚁𝚎𝚊𝚌𝚝𝚒𝚘𝚗 𝙱𝚘𝚝 𝚠𝚒𝚝𝚑 𝚂𝚘𝚖𝚎 𝚃𝚒𝚖𝚎𝚙𝚊𝚊𝚜 𝙶𝚊𝚖𝚎
-𝙾𝙽𝙻𝚈 𝙼𝚊𝚔𝚎 𝙼𝚎 𝙰𝚍𝚖𝚒𝚗 𝙸𝚗 𝚈𝚘𝚞𝚛 𝙶𝚛𝚘𝚞𝚙 𝙾𝚛 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 𝙵𝚘𝚛 𝙼𝚊𝚐𝚒𝚌𝚊𝚕 𝚁𝚎𝚊𝚌𝚝𝚒𝚘𝚗𝚜 💞
-𝙾𝚛 𝙳𝚘
-/𝚍𝚒𝚌𝚎  
-/𝚝𝚑𝚛𝚘𝚠 ....
-𝚎𝚝𝚌 𝚖𝚘𝚛𝚎 𝚌𝚖𝚗𝚍𝚜 𝚠𝚑𝚒𝚌𝚑 𝚜𝚑𝚘𝚠 𝚞 𝚒𝚗 𝚜𝚝𝚊𝚛 𝚝𝚎𝚡𝚝 𝚊𝚗𝚍 𝚎𝚗𝚓𝚘𝚢</b>
-<b><blockquote>Maintained By <a href='https://t.me/hgbotz'>𝙷𝙶𝙱𝙾𝚃ᶻ 🦋</a></blockquote></b>"""
+    HELP_TXT = """<b><blockquote>Maintained By <a href='https://t.me/hgbotz'>𝙷𝙶𝙱𝙾𝚃ᶻ 🦋</a></blockquote></b>"""
 
-    HOME_TXT = """ɪ ᴀᴍ sɪᴍᴘʟᴇ ʙᴜᴛ ᴘᴏᴡᴇʀꜰᴜʟʟ ᴀᴜᴛᴏ ʀᴇᴀᴄᴛɪᴏɴ ʙᴏᴛ.
-ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴀs ᴀ ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴏʀ ɢʀᴏᴜᴘ ᴛʜᴇɴ sᴇᴇ ᴍʏ ᴘᴏᴡᴇʀ
-<b><blockquote expandable>For Fun Use These Commands
-◉ /dice
-◉ /arrow
-◉ /goal
-◉ /luck
-◉ /throw
-◉ /bowling
-◉ /tenpins</blockquote></b>
+    HOME_TXT = """
 <b><blockquote>Maintained By <a href='https://t.me/hgbotz'>𝙷𝙶𝙱𝙾𝚃ᶻ 🦋</a></blockquote></b>"""
