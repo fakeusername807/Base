58488d67f9b1f965c7352f7e32f34fe09a8d8a42
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2025-05-25 13:09:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,20 @@
-# (c) @RknDeveloperr
-# Rkn Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Botz
-# Developer @RknDeveloperr
+
 
 import re, time, os
 from os import environ
 
-CRIC_API_KEY = "79261425-9b54-4e25-850c-c13b6e90b101"
 
 id_pattern = re.compile(r'^.\d+$')
 
 AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002166149059').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
-class Rkn_Bots(object):
+class HgBotz(object):
     
-    # Rkn client config  ( required.. 😥)
-    API_ID = os.environ.get("API_ID", "25492855")
-    API_HASH = os.environ.get("API_HASH", "61876db014de51a4ace6b169608be4f1")
+    
+    API_ID = os.environ.get("API_ID", "")
+    API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-    # start_pic
-    RKN_PIC = os.environ.get("RKN_PIC", "https://i.ibb.co/dMJFrqQ/photo-2024-09-25-11-37-05-7435920937339322372.jpg")
 
     # wes response configuration
     BOT_UPTIME = time.time()
@@ -34,8 +27,6 @@ class Rkn_Bots(object):
     DB_NAME = os.environ.get("DB_NAME", "HGBOTZ")     
     DB_URL = os.environ.get("DB_URL", "")
 
-    # default caption 
-    DEF_CAP = os.environ.get("DEF_CAP", "")
 
     # sticker Id
     STICKER_ID = os.environ.get("STICKER_ID", "CAACAgIAAxkBAAELFqBllhB70i13m-woXeIWDXU6BD2j7wAC9gcAAkb7rAR7xdjVOS5ziTQE")
@@ -43,8 +34,3 @@ class Rkn_Bots(object):
     # admin id  ( required.. 😥)
     ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6359874284 7609503336').split()]
     
-
-# Rkn Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Botz
-# Developer @RknDeveloperr
