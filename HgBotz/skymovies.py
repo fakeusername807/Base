1587c324f66239cfbbccf9a046c939884d628f1a
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2025-07-14 18:47:33 +0530

    Update skymovies.py

--- HgBotz/skymovies.py
+++ HgBotz/skymovies.py
@@ -200,13 +200,13 @@ async def process_and_send_movie(client: Client, movie_url: str):
 
         # Step 4: Format message
         text = "<b>🎬 New Movie Added! ✅</b>\n\n"
-        text += f"<b>Title 💫</b>: <code>{title}</code>\n\n" 
+        text += f"<b>Title 💫</b>: <code>{title}</code>\n" 
         if gofile_links:
             text += "\n<b>🔰GoFile Link🔰 (Directly Leech)</b>\n"
             for i, link in enumerate(gofile_links, 1):
-                text += f"• {link}\n"
+                text += f"• {link}\n\n"
               
-        text += f"<b>🐬Stream Tape Link🐬</b>\n {watch_url} \n"
+        text += f"<b>🐬Stream Tape Link🐬</b>\n {watch_url} \n\n"
           
         if normal_links:
             text += "<b>🍿 All Cloud Urls 🍿</b>\n"
