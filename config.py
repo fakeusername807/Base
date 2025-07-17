9104e801134a48e32fdec2dc6bf3bc390d696e04
MS DHONI <137499419+sbplays10@users.noreply.github.com>
2025-07-17 11:29:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,17 @@
-
-
 import re, time, os
 from os import environ
 
 
 id_pattern = re.compile(r'^.\d+$')
 
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002166149059').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002069524231').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
 class HgBotz(object):
     
     
-    API_ID = os.environ.get("API_ID", "25492855")
-    API_HASH = os.environ.get("API_HASH", "61876db014de51a4ace6b169608be4f1")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7999210242:AAE2cNUSIbubvCHY1Uy6LslOsLT2rcD4Mpk")
+    API_ID = os.environ.get("API_ID", "26334970")
+    API_HASH = os.environ.get("API_HASH", "e7d1141cca9fbe1ab45804163b5080c8")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7651584809:AAHEsTM3ALa4VWWPb_NmqFcL9QfsjvljNfY")
 
 
     # wes response configuration
@@ -21,13 +19,12 @@ class HgBotz(object):
     PORT = int(os.environ.get("PORT", "8080"))
 
     # force subs channel ( required.. 😥)
-    FORCE_SUB = os.environ.get("FORCE_SUB", "hgbotz") 
+    FORCE_SUB = os.environ.get("FORCE_SUB", "MrSagarbots") 
     
     # database config ( required.. 😥)
-    DB_NAME = os.environ.get("DB_NAME", "Postersbot")     
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://harsh:gunnu@cluster0.0uqkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "MrSagarbots")     
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://poster01:poster01@cluster0.f6hzzci.mongodb.net/?retryWrites=true&w=majority")
 
 
     # admin id  ( required.. 😥)
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6359874284').split()]
-    
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7965786027 5355635400').split()]
